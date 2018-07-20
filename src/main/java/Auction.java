789196c74637648672e439fb8fbc58b1c3bb462d
Blysku <blyskx@gmail.com>
2018-07-20 18:51:56 +0200

    Auction class in progress

---
+++ src/main/java/Auction.java
@@ -0,0 +1,59 @@
+import exceptions.OfferTooLowException;
+import models.Offer;
+
+import java.util.LinkedList;
+
+public class Auction {
+    private User owner;
+    private LinkedList<Offer> offersList;
+    private Offer lastOffer;
+    private String discription;
+    private String title;
+    private Product product;
+    private double price;
+
+    public Auction(User owner, String discription, String title, Product product, double price) {
+        this.owner = owner;
+        this.discription = discription;
+        this.title = title;
+        this.product = product;
+        this.price = price;
+        this.offersList = new LinkedList<>();
+        this.lastOffer = new Offer();
+    }
+
+    public void setNewOffer(Offer newOffer) throws OfferTooLowException, NullPointerException {
+        if (newOffer == null){
+            throw new NullPointerException();
+        }
+        if (this.lastOffer == null || this.lastOffer.getPrice() < newOffer.getPrice()){
+            this.lastOffer = newOffer;
+            this.offersList.push(newOffer);
+        }
+        throw new OfferTooLowException();
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public LinkedList<Offer> getOffersList() {
+        return offersList;
+    }
+
+    public Offer getLastOffer() {
+        return lastOffer;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
---
+++ src/main/java/exceptions/OfferTooLowException.java
@@ -0,0 +1,4 @@
+package exceptions;
+
+public class OfferTooLowException extends Exception {
+}
