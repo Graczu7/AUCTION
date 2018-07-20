d403d4daf0b93b1944bc6bd0bc6ef6e4b88d990e
GG <graczu7@wp.pl>
2018-07-20 18:40:38 +0200

    Zapis

---
+++ src/main/java/Users.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Users {
+
+    private String name;
+    private String login;
+    private String password;
+
+    public Users(String name, String login, String password) {
+        this.name = name;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public class setPassword {
+        Scanner sc = new Scanner(System.in);
+        boolean valid = false;
+        String password;
+
+    }
+}
+
