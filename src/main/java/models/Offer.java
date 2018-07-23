4988557967b3d8823bb8dc9657297245f0aaa6d2
marci <marcinpikul87@gmail.com>
2018-07-23 20:30:43 +0200

    mod Offer

--- src/main/java/models/Offer.java
+++ src/main/java/models/Offer.java
@@ -1,56 +1,30 @@
 package models;
 
+import java.math.BigDecimal;
 import java.util.Objects;
 
 public class Offer {
 
-    private String user;
-    private String objectToSell;
-    private String description;
-    private Double price;
+    private User user;
+    private Auction objectToSell;
+    private BigDecimal price;
 
-    public Offer() {
-    }
-
-    public Offer(String user, String objectToSell, String description, Double price) {
+    public Offer(User user, Auction objectToSell, BigDecimal price) {
         this.user = user;
         this.objectToSell = objectToSell;
-        this.description = description;
         this.price = price;
     }
 
-    public String getUser() {
-        return user;
-    }
 
-    public void setUser(String user) {
-        this.user = user;
-    }
 
-    public String getObjectToSell() {
-        return objectToSell;
+    @Override
+    public String toString() {
+        return "Offer{" +
+                "user=" + user +
+                ", objectToSell=" + objectToSell +
+                ", price=" + price +
+                '}';
     }
-
-    public void setObjectToSell(String objectToSell) {
-        this.objectToSell = objectToSell;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
 
     @Override
     public boolean equals(Object o) {
@@ -59,24 +33,42 @@ public class Offer {
         Offer offer = (Offer) o;
         return Objects.equals(user, offer.user) &&
                 Objects.equals(objectToSell, offer.objectToSell) &&
-                Objects.equals(description, offer.description) &&
                 Objects.equals(price, offer.price);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(user, objectToSell, description, price);
+        return Objects.hash(user, objectToSell, price);
     }
 
 
-    @Override
-    public String toString() {
-        return "Offer{" +
-                "user='" + user + '\'' +
-                ", objectToSell='" + objectToSell + '\'' +
-                ", description='" + description + '\'' +
-                ", price=" + price +
-                '}';
+
+    public User getUser() {
+        return user;
     }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Auction getObjectToSell() {
+        return objectToSell;
+    }
+
+    public void setObjectToSell(Auction objectToSell) {
+        this.objectToSell = objectToSell;
+    }
+
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+
+
 }
