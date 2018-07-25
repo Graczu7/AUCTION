3bbb9bca5c2e24969215d4ef085418ee08fc0831
Robert Kowal <robertk.krakow@gmail.com>
2018-07-25 19:17:59 +0200

    user file reader, writer

--- src/main/java/models/DataManager.java
+++ src/main/java/models/DataManager.java
@@ -1,9 +1,56 @@
 package models;
 
+import java.io.*;
+
 public class DataManager {
 
-    public void userFile(User user){
-        
+    public void userFileReader(User user) {
+
+        String fileName = "userfile.txt";
+        String line = null;
+
+        try {
+            FileReader fileReader = new FileReader(fileName);
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            bufferedReader.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + fileName + "'");
+        } catch (IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");
+
+        }
+    }
+
+    public void userFileWriter(User user){
+
+        String fileName = "userfile.txt";
+
+        try {
+
+            FileWriter fileWriter = new FileWriter(fileName);
+
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            // Note that write() does not automatically
+            // append a newline character.
+            bufferedWriter.write("Hello there,");
+            bufferedWriter.write(" here is some text.");
+            bufferedWriter.newLine();
+            bufferedWriter.write("We are writing");
+            bufferedWriter.write(" the text to the file.");
+
+            bufferedWriter.close();
+        }
+        catch(IOException ex) {
+            System.out.println("Error writing to file '" + fileName + "'");
+
+        }
     }
 
 }
